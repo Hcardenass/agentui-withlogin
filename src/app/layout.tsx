89ca6c4d77800ddf31ea9279751f22d22bc5613f
Adrian Cardenas <92567314+Hcardenass@users.noreply.github.com>
2025-05-29 01:28:21 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from 'react';
 import AuthProvider from './AuthProvider';
 
 export const metadata = {
-  title: 'TecnoQuery🤖',
+  title: '🤖TecnoQuery',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
