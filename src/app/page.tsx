899d85419340bc765ee5b2f3ea89b2e353dcffe3
Adrian Cardenas <92567314+Hcardenass@users.noreply.github.com>
2025-05-31 19:38:20 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default function Page() {
               className={`p-3 rounded max-w-[70%] whitespace-pre-wrap flex items-start gap-2 ${
                 m.de === 'usuario'
                   ? 'ml-auto bg-blue-100 text-black'
-                  : 'mr-auto bg-amber-100 text-amber-800'
+                  : 'mr-auto bg-cyan-100 text-cyan-900'
               }`}
             >
               {/* Icono de persona o robot */}
